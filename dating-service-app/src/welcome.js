9e4d54f42ad6783b3f0947ca7b06267b555a743b
DavidMuthama <156419416+DavidMuthama@users.noreply.github.com>
2024-05-21 15:09:51 +0300

    styling

--- dating-service-app/src/welcome.js
+++ dating-service-app/src/welcome.js
@@ -5,32 +5,34 @@ import "./welcome.css";
 
 export function Welcome() {
   const [input, setInput] = useState("");
-  // const [users, setUsers] = useState([]);
+  const [output, setOutput] = useState([]);
   const handleInput = async (e) => {
+    let newResponseEntries={}
     e.preventDefault();
     try {
       const response = await axios.post("/api/user/messages", { message: input });
       // alert(response.data);
       // alert(typeof(response.data))
       if (typeof response.data === 'string') {
-        alert(response.data);
+        newResponseEntries={text: response.data}
     }
-    else if (Array.isArray(response.data)) {
-      response.data.forEach(user => {
-          alert(`Name: ${user.name}, Age: ${user.age}, Phone: ${user.phone_no}`);
-      });
-  }
-    // Check if the `data` in response is an object
+  //   else if (Array.isArray(response.data)) {
+  //     response.data.forEach(user => {
+  //         alert(`Name: ${user.name}, Age: ${user.age}, Phone: ${user.phone_no}`);
+  //     });
+  // }
+  //   // Check if the `data` in response is an object
     else if (typeof response.data === 'object' && response.data !== null) {
         // Assuming the object might have keys like 'name', 'age', and 'message'
         // This is just an example, replace it with the actual keys from your response
+        newResponseEntries={text:response.data.message}
         const redirectUrl = `/api/user/${encodeURIComponent(response.data.name)}/messages`;
-        alert(response.data.message)
         window.location.href = redirectUrl;
     }
     } catch (error) {
       console.error("Error sending message:", error);
     }
+    setOutput([newResponseEntries])
   };
   return (
     <div className="MainPage">
@@ -45,6 +47,13 @@ export function Welcome() {
         <p>We are dedicated to helping you, YES you to find a good partner. So what are you waiting for, hop on and explore
           ALL that you want and find yourself someone. Don't be alone, don't feel alone.
         </p>
+      </div>
+      <div className="handleInput">
+      {output.map((message, index) => (
+              <div key={index} className={`chat-message ${message.from}`}>
+                {message.text}
+              </div>
+            ))}
       </div>
       <form onSubmit={handleInput} className="Myformy">
         <input
